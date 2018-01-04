fe496ea951b15546df8bdb32ff86bb6f732f58f6
Jack Q <QiaoBo@outlook.com>
2018-01-05 01:53:38 +0800

    add dialog theme

---
+++ app/overlay/dialog-overlay.scss.d.ts
@@ -0,0 +1,6 @@
+export const container: string;
+export const active: string;
+export const dialog: string;
+export const heading: string;
+export const message: string;
+export const actions: string;
--- app/overlay/dialog-overlay.tsx
+++ app/overlay/dialog-overlay.tsx
@@ -1,9 +1,21 @@
 import * as React from 'react';
 
+import * as styles from './dialog-overlay.scss';
+import { Button } from '../component/button';
+
 export class DialogOverlay extends React.Component {
   public render() {
     return (
-      <div onClick={() => console.log('dialog')}>Dialog Overlay</div>
+      <div className={`${styles.container} ${styles.active}`}>
+        <div className={styles.dialog}>
+          <div className={styles.heading}>{'Security Warning'}</div>
+          <div className={styles.message}>{'Content of the dialog'}</div>
+          <div className={styles.actions}>
+            <Button label="OK" />
+            <Button label="Cancel" />
+          </div>
+        </div>
+      </div>
     );
   }
 }

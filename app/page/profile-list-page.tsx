2975e05feb50bffc08b6969531460402ba845f62
TinkerAc <test767803@foxmail.com>
2018-01-07 03:46:55 +0800

    update profile-lsit-page style

--- app/page/profile-list-page.tsx
+++ app/page/profile-list-page.tsx
@@ -6,13 +6,12 @@ import { ISshProfile } from '../model/profile';
 import { ISshIdentity } from '../model/identity';
 import { ISshHostServer } from '../model/host-server';
 import { modelServiceConnector } from '../service/model-service';
-import { Button } from '../component/button';
 import { HostListPage } from './host-list-page';
 import { transitionService } from '../service/transition-service';
 import { pageService } from '../service/page-service';
 import { IdentityListPage } from './identity-list-page';
 import { createProfileEditPage } from './profile-edit-page';
-
+import { WelcomePage } from './welcome-page';
 interface IProps {
   profileList: ISshProfile[];
   hostList: ISshHostServer[];
@@ -23,7 +22,10 @@ class ProfileListPageView extends React.Component<IPageViewProps & IProps> {
   public render() {
     return (
       <div className={styles.container}>
-        <div className={styles.PageTitle}>Profile List Page</div>
+        <div className={styles.PageTitle}>
+        <button onClick={(e) => this.transitToWelcomePage(e)} className={styles.arrowBack}><i className="material-icons">arrow_back</i></button>
+          Profile List Page
+        </div>
         <div>
           {
             this.props.profileList.map((p) => (
@@ -41,13 +43,10 @@ class ProfileListPageView extends React.Component<IPageViewProps & IProps> {
             </div>
           }
         </div>
-        <div>
-          <Button label="new profile" onClick={(e) => this.transitPage(e, createProfileEditPage())} />
-          <Button label="host list" onClick={(e) => this.transitPage(e, HostListPage)} />
-          <Button label="identity list" onClick={(e) => this.transitPage(e, IdentityListPage)} />
-          <button><i className="material-icons">plus</i></button>
-          <button><i className="material-icons">person</i></button>
-          <button><i className="material-icons">person</i></button>
+        <div className={styles.btnGroup}>
+          <button className={styles.NavBtn} onClick={(e) => this.transitPage(e, createProfileEditPage())}><i className="material-icons">add_box</i></button>
+          <button className={styles.NavBtn} onClick={(e) => this.transitPage(e, HostListPage)}><i className="material-icons">computer</i></button>
+          <button className={styles.NavBtn} onClick={(e) => this.transitPage(e, IdentityListPage)}><i className="material-icons">person</i></button>
         </div>
       </div>
     );
@@ -58,6 +57,11 @@ class ProfileListPageView extends React.Component<IPageViewProps & IProps> {
       pageService.replaceTabPage(this.props.tabId, page);
     });
   }
+
+  private transitToWelcomePage(e: React.MouseEvent<Element>) {
+    this.transitPage(e, WelcomePage);
+  }
+
 }
 
 export const ProfileListPage = modelServiceConnector<IProps, IPageViewProps>(

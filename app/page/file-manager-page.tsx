d14935039d2637812d561bc6bf8c933bf4db6658
Jack Q <QiaoBo@outlook.com>
2018-01-07 07:10:33 +0800

    sftp page

--- app/page/file-manager-page.tsx
+++ app/page/file-manager-page.tsx
@@ -2,13 +2,57 @@ import * as React from 'react';
 
 import * as styles from './file-manager-page.scss';
 import { IPageViewProps } from '../model/page';
+import { connectionServiceConnector, connectionService } from '../service/connection-service';
+import { ISshProfile } from '../model/profile';
+import { getMessagePage } from './message-page';
+import { ISshConnection } from '../model/connection';
 
-export class FileManagerPage extends React.Component<IPageViewProps> {
+interface ISftpFile {
+  handle: string;
+  name: string;
+}
+interface IProps {
+  connection: ISshConnection;
+  processing: boolean;
+  currentPath: string;
+  fileList: ISftpFile[];
+}
+class FileManagerPageView extends React.Component<IPageViewProps & IProps> {
   public render() {
     return (
       <div className={styles.container}>
         File Manager Page
+        <div>
+          {this.props.currentPath}
+        </div>
+        <div>
+          {
+            this.props.fileList.map((f) => (
+              <div key={f.handle}>
+                {f.name}
+              </div>
+            ))
+          }
+        </div>
       </div>
     );
   }
- }
+}
+
+export const createFileManagerPage = (profile: ISshProfile) => {
+  const conn = connectionService.createSftpConnection(profile);
+  if (!conn) {
+    return getMessagePage('failed to start connection');
+  }
+
+  const FileManagerPage = connectionServiceConnector<IProps, IPageViewProps>(
+    (state, svc) => ({
+      connection: conn,
+      processing: false,
+      currentPath: '',
+      fileList: [],
+    }),
+    FileManagerPageView,
+  );
+  return FileManagerPage;
+};
